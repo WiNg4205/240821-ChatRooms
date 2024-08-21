d481568fe2e2e45e4763b962acb20f819ab39372
WiNg4205 <105526030+WiNg4205@users.noreply.github.com>
2024-08-21 17:28:35 +1000

    done

---
+++ frontend/components/Chat.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-52 w-72 relative bg-neutral-50 border rounded-lg hover:border-sky-200 hover:drop-shadow-md">
+    <font-awesome :icon="['fas', 'comment-dots']" class="text-sky-400 size-12 absolute top-4 left-4" />
+    <div class="bg-white rounded-lg absolute bottom-0 w-full px-4 py-1">
+      <h2 class="font-bold text-lg text-neutral-900">{{ title }}</h2>
+      <h3 class="font-semibold text-neutral-500">{{ description }}</h3>          
+    </div>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    title: String,
+    description: String
+  })
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col items-center">
+    <div class="w-full h-28 flex items-center justify-center border-b shadow-md gap-3">
+      <font-awesome :icon="['fas', 'bolt']" class="size-12 text-sky-400" />
+      <h1 class="text-6xl font-bold tracking-wide text-neutral-700">Chat rooms</h1>
+    </div>
+    
+    <div class="grid grid-cols-4 gap-6 p-16">
+      <Chat title="Epic chat" description="Interesting stuff here" />
+      <Chat title="good chat" description="some good stuff!" />
+      <Chat title="Educational chat" description="Learn here" />
+      <Chat title="Spam spam spam" description="spam spam spam" />
+      <Chat title="Intellectual chat" description="pretentious stuff here!" />
+      <Chat title="bad chat" description="don't join" />
+      <Chat title="Sports" description="self-explanatory" />
+      <Chat title="good chat" description="some good stuff!" />
+      <Chat title="good chat" description="some good stuff!" />
+      <Chat title="good chat" description="some good stuff!" />
+      <Chat title="good chat" description="some good stuff!" />
+      <Chat title="good chat" description="some good stuff!" />
+      <Chat title="good chat" description="some good stuff!" />
+    </div>
+    
+  </div>
+</template>
